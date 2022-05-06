c047dc39b9ffc70325b2d3952a3d20fa08bf7726
rashm1n <rashmin.ravindu@gmail.com>
2022-05-07 01:04:56 +0530

    Initial Commit

---
+++ bestreads-data-loader/src/main/java/com/rashmin/bestreadsdataloader/BestreadsDataLoaderApplication.java
@@ -0,0 +1,12 @@
+package com.rashmin.bestreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class BestreadsDataLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestreadsDataLoaderApplication.class, args);
+    }
+
+}
